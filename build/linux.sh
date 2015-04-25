90dc3478e0a4c8790b68a081941e4dedbbcc9cfa
Klaus Reimer <k@ailis.de>
2015-04-25 09:56:14 +0200

    Clean up linux build script

--- build/linux.sh
+++ build/linux.sh
@@ -1,105 +1,79 @@
-# ======================================================================
+# ============================================================================
 # Build script for Linux.
 #
-# The script automatically compiles the binary for the corresponding 
-# platform and creates the JAR file in the target folder.
+# The script automatically compiles the binary for the local architecture and
+# creates the corresponding JAR file in the target folder.
 #
-# Dependencies: cmake, make, curl
-# ======================================================================
+# Requirements: cmake, make, curl, jar
+# ============================================================================
 
+# Fail on all errors
 set -e
 
-
-# ----------------------------------------------------------------------
-# Setup variables
-# ----------------------------------------------------------------------
-
+# Software versions
 LIBUSB_VERSION=1.0.19
 EUDEV_VERSION=3.0
 
-CURRENT=$(realpath $(dirname $0))
-PROJECT_DIR="$CURRENT/.."
-OS=linux
-if [ "$(arch)" = "x86_64" ]
-then
-    ARCH=x86_64
-elif [ "$(arch)" = "armv7l" ]
-then
-    ARCH=arm    
-else
-    ARCH=x86
-fi
+# Determine directories
+cd "$(dirname $0)/.."
+PROJECT_DIR="$(pwd)"
 TARGET_DIR="$PROJECT_DIR/target"
-BUILD_DIR="$TARGET_DIR/build"
-DEPS_DIR="$TARGET_DIR/deps"
+ROOT_DIR="$TARGET_DIR/root"
 
-mkdir -p "$DEPS_DIR"
+# Clean up target directory
+rm -rf "$TARGET_DIR"
 
+# Determine OS and architecture
+OS=linux
+case "$(arch)" in
+    "x86_64")
+        ARCH=x86_64
+        ;;
+    "i"[3456]"86")
+        ARCH=x86
+        ;;
+    "armv"*)
+        ARCH=arm
+        ;;
+    *)
+        echo "Unknown platform: $(arch)"
+        exit 1
+esac
+echo "Building for platform $OS-$ARCH"
 
-# ----------------------------------------------------------------------
 # Download and build eudev
-# ----------------------------------------------------------------------
-
-EUDEV_NAME="eudev-$EUDEV_VERSION"
-EUDEV_ARCHIVE="$EUDEV_NAME.tar.gz"
-EUDEV_URL="http://dev.gentoo.org/~blueness/eudev/$EUDEV_ARCHIVE"
-cd "$DEPS_DIR"
-curl -L "$EUDEV_URL" | tar xvz
-cd "$EUDEV_NAME"
+mkdir -p "$TARGET_DIR/eudev"
+cd "$TARGET_DIR/eudev"
+curl -L "http://dev.gentoo.org/~blueness/eudev/eudev-$EUDEV_VERSION.tar.gz" \
+    | tar xvz --strip-components=1
 ./configure --disable-shared --enable-static --with-pic --prefix="" \
     --enable-split-usr --disable-manpages --disable-kmod \
     --disable-gudev --disable-selinux --disable-blkid
-make
-make install-strip DESTDIR="$DEPS_DIR"
+make install-strip DESTDIR="$ROOT_DIR"
 
-
-# ----------------------------------------------------------------------
 # Download and build libusb
-# ----------------------------------------------------------------------
-
-LIBUSB_NAME="libusb-$LIBUSB_VERSION"
-LIBUSB_ARCHIVE="$LIBUSB_NAME.tar.bz2"
-LIBUSB_URL="http://downloads.sf.net/project/libusb/libusb-1.0/$LIBUSB_NAME/$LIBUSB_ARCHIVE"
-cd "$DEPS_DIR"
-curl -L "$LIBUSB_URL" | tar xvj
-cd "$LIBUSB_NAME"
-CFLAGS="-I$DEPS_DIR/include" \
-LDFLAGS="-L$DEPS_DIR/lib" \
+mkdir -p "$TARGET_DIR/libusb"
+cd "$TARGET_DIR/libusb"
+curl -L "http://downloads.sf.net/project/libusb/libusb-1.0/libusb-$LIBUSB_VERSION/libusb-$LIBUSB_VERSION.tar.bz2" \
+    | tar xvj --strip-components=1
+CFLAGS="-I$ROOT_DIR/include" \
+LDFLAGS="-L$ROOT_DIR/lib" \
 ./configure --disable-shared --enable-static --with-pic --prefix=""
-make
-make install-strip DESTDIR="$DEPS_DIR"
+make install-strip DESTDIR="$ROOT_DIR"
 
-
-# ----------------------------------------------------------------------
 # Build libusb4java
-# ----------------------------------------------------------------------
-
-mkdir -p "$BUILD_DIR"
-cd "$BUILD_DIR"
+mkdir -p "$TARGET_DIR/libusb4java"
+cd "$TARGET_DIR/libusb4java"
 cmake "$PROJECT_DIR" \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX="" \
-    -DLibUsb_INCLUDE_DIR="$DEPS_DIR/include" \
-    -DLibUsb_LIBRARY="$DEPS_DIR/lib/libusb-1.0.a" \
-    -DLibUdev_INCLUDE_DIR="$DEPS_DIR/include" \
-    -DLibUdev_LIBRARY="$DEPS_DIR/lib/libudev.a"
-make
-make install/strip DESTDIR="$DEPS_DIR"
+    -DLibUsb_INCLUDE_DIR="$ROOT_DIR/include" \
+    -DLibUsb_LIBRARY="$ROOT_DIR/lib/libusb-1.0.a" \
+    -DLibUdev_INCLUDE_DIR="$ROOT_DIR/include" \
+    -DLibUdev_LIBRARY="$ROOT_DIR/lib/libudev.a"
+make install/strip DESTDIR="$ROOT_DIR"
 
-
-# ----------------------------------------------------------------------
 # Create the JAR file
-# ----------------------------------------------------------------------
-
-mkdir -p "classes/org/usb4java/$OS-$ARCH"
-cp src/libusb4java.so classes/org/usb4java/$OS-$ARCH/
-jar cf "$TARGET_DIR/libusb4java-$OS-$ARCH.jar" -C classes org
-
-
-# ----------------------------------------------------------------------
-# Clean up and go back to original directory
-# ----------------------------------------------------------------------
-
-cd $CURRENT
-rm -rf "$BUILD_DIR"
-rm -rf "$DEPS_DIR"
+mkdir -p "$TARGET_DIR/classes/org/usb4java/$OS-$ARCH"
+cp "$ROOT_DIR/lib/libusb4java.so" "$TARGET_DIR/classes/org/usb4java/$OS-$ARCH"
+jar cf "$TARGET_DIR/libusb4java-$OS-$ARCH.jar" -C "$TARGET_DIR/classes" org
