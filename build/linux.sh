88e3492497873569c35abb5e26443c560bbd27a7
Klaus Reimer <k@ailis.de>
2016-01-24 01:02:58 +0100

    Use eudev 3.1.5 distribution tarball and cache downloaded artifacts

--- build/linux.sh
+++ build/linux.sh
@@ -12,16 +12,21 @@ set -e
 
 # Software versions
 LIBUSB_VERSION=1.0.20
-EUDEV_VERSION=3.1.4
+EUDEV_VERSION=3.1.5
 
 # Determine directories
 cd "$(dirname $0)/.."
 PROJECT_DIR="$(pwd)"
 TARGET_DIR="$PROJECT_DIR/target"
-ROOT_DIR="$TARGET_DIR/root"
+BUILD_DIR="$TARGET_DIR/build"
+DOWNLOAD_DIR="$TARGET_DIR/downloads"
+ROOT_DIR="$BUILD_DIR/root"
 
-# Clean up target directory
-rm -rf "$TARGET_DIR"
+# Clean up build directory
+rm -rf "$BUILD_DIR"
+
+# Create download directory if not already present
+mkdir -p "$DOWNLOAD_DIR"
 
 # Determine OS and architecture
 OS=linux
@@ -41,30 +46,43 @@ case "$(arch)" in
 esac
 echo "Building for platform $OS-$ARCH"
 
-# Download and build eudev
-mkdir -p "$TARGET_DIR/eudev"
-cd "$TARGET_DIR/eudev"
-curl -L "https://github.com/gentoo/eudev/archive/v$EUDEV_VERSION.tar.gz" \
-    | tar xvz --strip-components=1
-./autogen.sh
+# Download, unpack and compile eudev
+EUDEV_TARBALL="eudev-$EUDEV_VERSION.tar.gz"
+EUDEV_SOURCE="http://dev.gentoo.org/~blueness/eudev/$EUDEV_TARBALL"
+EUDEV_TARGET="$DOWNLOAD_DIR/$EUDEV_TARBALL"
+if [ ! -f "$EUDEV_TARGET" ]
+then
+    curl -C - -o "$EUDEV_TARGET.download" -L "$EUDEV_SOURCE"
+    mv -f "$EUDEV_TARGET.download" "$EUDEV_TARGET"
+fi
+mkdir -p "$BUILD_DIR/eudev"
+cd "$BUILD_DIR/eudev"
+tar xvf "$EUDEV_TARGET" --strip-components=1
 ./configure --disable-shared --enable-static --with-pic --prefix="" \
     --enable-split-usr --disable-manpages --disable-kmod \
     --disable-gudev --disable-selinux --disable-blkid
 make install-strip DESTDIR="$ROOT_DIR"
 
 # Download and build libusb
-mkdir -p "$TARGET_DIR/libusb"
-cd "$TARGET_DIR/libusb"
-curl -L "http://downloads.sf.net/project/libusb/libusb-1.0/libusb-$LIBUSB_VERSION/libusb-$LIBUSB_VERSION.tar.bz2" \
-    | tar xvj --strip-components=1
+LIBUSB_TARBALL="libusb-$LIBUSB_VERSION.tar.bz2"
+LIBUSB_SOURCE="http://downloads.sf.net/project/libusb/libusb-1.0/libusb-$LIBUSB_VERSION/$LIBUSB_TARBALL"
+LIBUSB_TARGET="$DOWNLOAD_DIR/$LIBUSB_TARBALL"
+if [ ! -f "$LIBUSB_TARGET" ]
+then
+    curl -C - -o "$LIBUSB_TARGET.download" -L "$LIBUSB_SOURCE"
+    mv -f "$LIBUSB_TARGET.download" "$LIBUSB_TARGET"
+fi
+mkdir -p "$BUILD_DIR/libusb"
+cd "$BUILD_DIR/libusb"
+tar xvf "$LIBUSB_TARGET" --strip-components=1
 CFLAGS="-I$ROOT_DIR/include" \
 LDFLAGS="-L$ROOT_DIR/lib" \
 ./configure --disable-shared --enable-static --with-pic --prefix=""
 make install-strip DESTDIR="$ROOT_DIR"
 
 # Build libusb4java
-mkdir -p "$TARGET_DIR/libusb4java"
-cd "$TARGET_DIR/libusb4java"
+mkdir -p "$BUILD_DIR/libusb4java"
+cd "$BUILD_DIR/libusb4java"
 cmake "$PROJECT_DIR" \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX="" \
@@ -74,6 +92,6 @@ cmake "$PROJECT_DIR" \
 make install/strip DESTDIR="$ROOT_DIR"
 
 # Create the JAR file
-mkdir -p "$TARGET_DIR/classes/org/usb4java/$OS-$ARCH"
-cp "$ROOT_DIR/lib/libusb4java.so" "$TARGET_DIR/classes/org/usb4java/$OS-$ARCH"
-jar cf "$TARGET_DIR/libusb4java-$OS-$ARCH.jar" -C "$TARGET_DIR/classes" org
+mkdir -p "$BUILD_DIR/classes/org/usb4java/$OS-$ARCH"
+cp "$ROOT_DIR/lib/libusb4java.so" "$BUILD_DIR/classes/org/usb4java/$OS-$ARCH"
+jar cf "$BUILD_DIR/libusb4java-$OS-$ARCH.jar" -C "$BUILD_DIR/classes" org
